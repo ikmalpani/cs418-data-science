3e36fe0f42f4357a86e85c8b1baca3805feac17a
Yashika Goyal <36175517+iyashikagoyal@users.noreply.github.com>
2018-04-17 20:06:34 -0500

    Update avg_rev_rating.py

--- inspection_review_rel/avg_rev_rating.py
+++ inspection_review_rel/avg_rev_rating.py
@@ -6,7 +6,7 @@ temp = final.groupby(["restaurantID"] ,axis=0)[['rating']].mean()
 rating = pd.DataFrame(temp)
 rating.to_csv('restID_rating.csv')
 r = pd.read_csv('restID_rating.csv')
-yelp_rest = pd.read_csv('/Users/yashikagoyal/Desktop/yelp_data/restaurants_60601-60606.csv')
+yelp_rest = pd.read_csv('restaurants_60601-60606.csv')
 
 yelp_avg = pd.merge(yelp_rest, r, on= 'restaurantID', how='inner')
 
